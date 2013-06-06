5b7251edc63ed146e90f8e4c32cb555f86701b3e
elmadjian <cadu_elmadjian@yahoo.com.br>
2013-06-06 14:32:14 -0300

    adicionando graficos.*

---
+++ graficos.h
@@ -0,0 +1,29 @@
+/*
+ \__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__
+
+Nomes:
+    Ricardo Mikio Morita (N.USP: 5412562)
+    Carlos Eduardo Elmadjian (N.USP: 5685741)
+    Gil Santaella (N.USP: 6883598)
+
+Profs:
+    Kelly Rosa Braghetto
+
+Arquivo:
+    graficos.h
+
+Materia:
+    Canoagem    
+
+Referencias:
+	Muitas paginas do man
+
+ \__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__\__
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+
+void pintaRio();
